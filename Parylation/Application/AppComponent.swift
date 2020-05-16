285c28e06b712f6916a596b017780f96c9567b4a
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2020-05-17 02:53:01 +0300

    Implemented project tree

---
+++ Parylation/Application/AppComponent.swift
@@ -0,0 +1,17 @@
+//
+//  AppComponent.swift
+//  Parylation
+//
+//  Created by Vladislav Kondrashkov on 5/17/20.
+//  Copyright © 2020 Vladislav Kondrashkov. All rights reserved.
+//
+
+import UIKit
+
+final class AppComponent {
+    unowned let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+}
---
+++ Parylation/Application/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Parylation
+//
+//  Created by Vladislav Kondrashkov on 5/17/20.
+//  Copyright © 2020 Vladislav Kondrashkov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
