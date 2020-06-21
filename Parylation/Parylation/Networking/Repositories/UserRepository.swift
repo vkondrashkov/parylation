fc12a8cdb691ba94fe2310cb7ee48f5f64ff22a1
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2020-06-22 00:16:22 +0300

    PI-003 Implemented UserRepository

--- Parylation/Parylation/Networking/Repositories/UserRepository.swift
+++ Parylation/Parylation/Networking/Repositories/UserRepository.swift
@@ -19,44 +19,24 @@ final class UserRepositoryImpl: UserRepository {
     }
     
     func registerUser(login: String, password: String) -> Signal<Void, UserRepositoryError> {
-        let registerUser = PassthroughSubject<Void, UserRepositoryError>()
-        provider.request(
-            .signUp(login: login, password: password),
-            completion: { response in
-                switch response {
-                case .success:
-                    registerUser.receive()
-                    registerUser.receive(completion: .finished)
-                case .failure:
-                    registerUser.receive(completion: .failure(.failed))
-                }
+        return provider.reactive
+            .request(.signUp(login: login, password: password))
+            .eraseType()
+            .mapError { _ in
+                return .failed
             }
-        )
-        return registerUser.toSignal()
     }
     
     func authorizeUser(login: String, password: String) -> Signal<User, UserRepositoryError> {
-        let authorizeUser = PassthroughSubject<User, UserRepositoryError>()
-        provider.request(
-            .signIn(login: login, password: password),
-            completion: { response in
-                switch response {
-                case .success(let data):
-                    guard let json = try? data.mapJSON() as? [String: Any] else {
-                        authorizeUser.receive(completion: .failure(.failed))
-                        return
-                    }
-                    guard let mappableUser = Mapper<MappableUser>().map(JSON: json) else {
-                        authorizeUser.receive(completion: .failure(.missingData))
-                        return
-                    }
-                    authorizeUser.receive(mappableUser.toDomain())
-                    authorizeUser.receive(completion: .finished)
-                case .failure:
-                    authorizeUser.receive(completion: .failure(.failed))
-                }
+        let authorizeUser: Signal<MappableUser, UserRepositoryError> = provider.reactive
+            .request(.signIn(login: login, password: password))
+            .mapError { _ -> UserRepositoryError in
+                return .failed
             }
-        )
-        return authorizeUser.toSignal()
+        
+        return authorizeUser
+            .map {
+                $0.toDomain()
+            }
     }
 }
