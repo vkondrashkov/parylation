f7d33ce6283557dc87884a004af0a0a35b4b9ec8
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2021-01-18 00:34:10 +0300

    PI-019 Added SignOut feature

--- ParylationDomain/ParylationDomain/Interactors/SettingsInteractor.swift
+++ ParylationDomain/ParylationDomain/Interactors/SettingsInteractor.swift
@@ -14,13 +14,24 @@ public enum SettingsInteractorError: Error {
     case failed
 }
 
-public protocol SettingsInteractor { }
+public protocol SettingsInteractor {
+    func signout() -> Single<Void>
+}
 
 public final class SettingsInteractorImpl {
-    public init() { }
+    private let authorizationUseCase: AuthorizationUseCase
+
+    public init(authorizationUseCase: AuthorizationUseCase) {
+        self.authorizationUseCase = authorizationUseCase
+    }
 }
 
 // MARK: - SettingsInteractor implementation
 
-extension SettingsInteractorImpl: SettingsInteractor { }
+extension SettingsInteractorImpl: SettingsInteractor {
+    public func signout() -> Single<Void> {
+        return authorizationUseCase.signout()
+            .catchError { _ in .error(SettingsInteractorError.failed) }
+    }
+}
 
