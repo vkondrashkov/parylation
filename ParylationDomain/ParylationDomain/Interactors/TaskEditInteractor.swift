bf5065129c1e2a9112c464266e516f6bf61faab0
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2021-01-21 22:17:38 +0300

    PI-031 Added repositories to Home, TaskEdit and Task interactors

--- ParylationDomain/ParylationDomain/Interactors/TaskEditInteractor.swift
+++ ParylationDomain/ParylationDomain/Interactors/TaskEditInteractor.swift
@@ -14,6 +14,8 @@ public enum TaskEditInteractorError: Error {
 
 public protocol TaskEditInteractor {
     func fetchTask(taskId: String) -> Single<Task>
+    func fetchIcons() -> Single<[Icon]>
+    func fetchColors() -> Single<[Color]>
     func save(task: Task) -> Single<Void>
     func validate(title: String) -> Single<Bool>
     func validate(description: String) -> Single<Bool>
@@ -22,15 +24,21 @@ public protocol TaskEditInteractor {
 
 public final class TaskEditInteractorImpl {
     private let taskRepository: TaskRepository
+    private let iconRepository: IconRepository
+    private let colorRepository: ColorRepository
     private let credentialsValidatorService: CredentialsValidatorService
     private let pushNotificationsService: PushNotificationsService
 
     public init(
         taskRepository: TaskRepository,
+        iconRepository: IconRepository,
+        colorRepository: ColorRepository,
         credentialsValidatorService: CredentialsValidatorService,
         pushNotificationsService: PushNotificationsService
     ) {
         self.taskRepository = taskRepository
+        self.iconRepository = iconRepository
+        self.colorRepository = colorRepository
         self.credentialsValidatorService = credentialsValidatorService
         self.pushNotificationsService = pushNotificationsService
     }
@@ -43,10 +51,22 @@ extension TaskEditInteractorImpl: TaskEditInteractor {
         return taskRepository.fetchTask(taskId: taskId)
             .catchErrorJustReturn(Task(
                 id: UUID().uuidString,
+                iconId: "",
+                colorId: "",
                 title: "",
                 taskDescription: "",
                 date: Date()
             ))
+    }
+
+    public func fetchIcons() -> Single<[Icon]> {
+        return iconRepository.fetchAll()
+            .catchError { _ in return .error(TaskEditInteractorError.failed) }
+    }
+
+    public func fetchColors() -> Single<[Color]> {
+        return colorRepository.fetchAll()
+            .catchError { _ in return .error(TaskEditInteractorError.failed) }
     }
 
     public func save(task: Task) -> Single<Void> {
--- ParylationDomain/ParylationDomain/Interactors/TaskInteractor.swift
+++ ParylationDomain/ParylationDomain/Interactors/TaskInteractor.swift
@@ -14,14 +14,24 @@ public enum TaskInteractorError: Error {
 
 public protocol TaskInteractor {
     func fetchTask(taskId: String) -> Single<Task>
+    func fetchIcon(id: String) -> Single<Icon>
+    func fetchColor(id: String) -> Single<Color>
     func deleteTask(taskId: String) -> Single<Void>
 }
 
 public final class TaskInteractorImpl {
     private let taskRepository: TaskRepository
+    private let iconRepository: IconRepository
+    private let colorRepository: ColorRepository
 
-    public init(taskRepository: TaskRepository) {
+    public init(
+        taskRepository: TaskRepository,
+        iconRepository: IconRepository,
+        colorRepository: ColorRepository
+    ) {
         self.taskRepository = taskRepository
+        self.iconRepository = iconRepository
+        self.colorRepository = colorRepository
     }
 }
 
@@ -31,6 +41,16 @@ extension TaskInteractorImpl: TaskInteractor {
     public func fetchTask(taskId: String) -> Single<Task> {
         return taskRepository.fetchTask(taskId: taskId)
             .catchError { _ in return .error(TaskInteractorError.failed) }
+    }
+
+    public func fetchIcon(id: String) -> Single<Icon> {
+        return iconRepository.fetch(id: id)
+            .catchError { _ in return .error(TaskInteractorError.failed) }
+    }
+
+    public func fetchColor(id: String) -> Single<Color> {
+        return colorRepository.fetch(id: id)
+            .catchError { _ in return .error(TaskEditInteractorError.failed) }
     }
 
     public func deleteTask(taskId: String) -> Single<Void> {
