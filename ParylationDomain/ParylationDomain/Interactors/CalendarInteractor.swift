678164564408f2d8200aec57ce8bedfa0f3a62fe
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2021-02-03 21:36:16 +0300

    PI-017 Added MonthMetadata generator

--- ParylationDomain/ParylationDomain/Interactors/CalendarInteractor.swift
+++ ParylationDomain/ParylationDomain/Interactors/CalendarInteractor.swift
@@ -12,13 +12,54 @@ public enum CalendarInteractorError: Error {
     case failed
 }
 
-public protocol CalendarInteractor { }
+public protocol CalendarInteractor {
+    func monthMetadata(date: Date) -> Single<MonthMetadata>
+}
 
 public final class CalendarInteractorImpl {
-    public init() { }
+    private let calendar: Calendar
+
+    public init(
+        calendar: Calendar
+    ) {
+        self.calendar = calendar
+    }
 }
 
 // MARK: - CalendarInteractor implementation
 
-extension CalendarInteractorImpl: CalendarInteractor { }
+extension CalendarInteractorImpl: CalendarInteractor {
+    public func monthMetadata(date: Date) -> Single<MonthMetadata> {
+        return .create { [weak self] single in
+            guard let self = self else {
+                single(.error(CalendarInteractorError.failed))
+                return Disposables.create()
+            }
+            let numberOfDaysInMonth = self.calendar.range(of: .day, in: .month, for: date)?.count
+            let firstDayOfMonth = self.calendar.date(from: self.calendar.dateComponents([.year, .month], from: date))
+
+            guard let numberOfDays = numberOfDaysInMonth, let firstDay = firstDayOfMonth else {
+                single(.error(CalendarInteractorError.failed))
+                return Disposables.create()
+            }
+
+            let lastDayInMonth = self.calendar.date(byAdding: DateComponents(month: 1, day: -1), to: firstDay)
+            let firstDayWeekday = self.calendar.component(.weekday, from: firstDay)
+
+            guard let lastDay = lastDayInMonth else {
+                single(.error(CalendarInteractorError.failed))
+                return Disposables.create()
+            }
+
+            let metadata = MonthMetadata(
+                numberOfDays: numberOfDays,
+                firstDay: firstDay,
+                lastDay: lastDay,
+                firstDayWeekday: firstDayWeekday
+            )
+            single(.success(metadata))
+            return Disposables.create()
+        }
+    }
+}
 
