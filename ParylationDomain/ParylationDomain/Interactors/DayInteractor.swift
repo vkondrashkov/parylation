584a455fcd473e94828ead82cf01b4d1938aadaa
Vladislav Kondrashkov <vladislav.kondrashkov@gmail.com>
2021-02-11 11:55:44 +0300

    PI-036 Implemented Interactor

--- ParylationDomain/ParylationDomain/Interactors/DayInteractor.swift
+++ ParylationDomain/ParylationDomain/Interactors/DayInteractor.swift
@@ -12,13 +12,50 @@ public enum DayInteractorError: Error {
     case failed
 }
 
-public protocol DayInteractor { }
+public protocol DayInteractor {
+    func fetchTaks() -> Single<[Task]>
+    func fetchIcon(id: String) -> Single<Icon>
+    func fetchColor(id: String) -> Single<Color>
+    func deleteTask(taskId: String) -> Single<Void>
+}
 
 public final class DayInteractorImpl {
-    public init() { }
+    private let taskRepository: TaskRepository
+    private let iconRepository: IconRepository
+    private let colorRepository: ColorRepository
+
+    public init(
+        taskRepository: TaskRepository,
+        iconRepository: IconRepository,
+        colorRepository: ColorRepository
+    ) {
+        self.taskRepository = taskRepository
+        self.iconRepository = iconRepository
+        self.colorRepository = colorRepository
+    }
 }
 
 // MARK: - DayInteractor implementation
 
-extension DayInteractorImpl: DayInteractor { }
+extension DayInteractorImpl: DayInteractor {
+    public func fetchTaks() -> Single<[Task]> {
+        return taskRepository.fetchTasks()
+            .catchError { _ in .error(DayInteractorError.failed) }
+    }
+
+    public func fetchIcon(id: String) -> Single<Icon> {
+        return iconRepository.fetch(id: id)
+            .catchError { _ in .error(DayInteractorError.failed) }
+    }
+
+    public func fetchColor(id: String) -> Single<Color> {
+        return colorRepository.fetch(id: id)
+            .catchError { _ in .error(DayInteractorError.failed) }
+    }
+
+    public func deleteTask(taskId: String) -> Single<Void> {
+        return taskRepository.deleteTask(taskId: taskId)
+            .catchError { _ in .error(DayInteractorError.failed) }
+    }
+}
 
